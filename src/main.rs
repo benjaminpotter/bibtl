b35426c8703b29da07a7bbb2c30d85de73960527
Ben Potter <bp@benpot.com>
2026-02-23 12:38:28 -0500

    add deps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, Subcommand};
+use ratatui::{DefaultTerminal, Frame};
+use std::path::PathBuf;
+
+fn main() -> color_eyre::Result<()> {
+    let cli = Cli::parse();
+
+    color_eyre::install()?;
+    ratatui::run(app)?;
+    Ok(())
+}
+
+fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if crossterm::event::read()?.is_key_press() {
+            break Ok(());
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("hello world", frame.area());
+}
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: BibtlCommand,
+}
+
+#[derive(Subcommand)]
+enum BibtlCommand {
+    Dedup {
+        #[arg(required = true, value_name = "FILE", num_args = 1..)]
+        bib_paths: Vec<PathBuf>,
+    },
+    Review {
+        #[arg(required = true, value_name = "FILE")]
+        bib_path: PathBuf,
+    },
 }
