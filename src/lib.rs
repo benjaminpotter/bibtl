746b36576610511ec079ef7c3b52d4472507601e
Ben Potter <bp@benpot.com>
2026-02-23 13:52:40 -0500

    play with biblatex

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+#[derive(Clone, Debug, PartialEq, Hash)]
+pub struct Entry {
+    doi: String,
+    title: String,
+    abstr: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use biblatex::{Bibliography, ChunksExt};
+
+    #[test]
+    fn load_bibtex() {
+        let src = "@book{tolkien1937, author = {J. R. R. Tolkien}, title = {The Lord of the Rings}, abstract = {An adventure!}}";
+        let bibliography = Bibliography::parse(src).unwrap();
+        let entry = bibliography.get("tolkien1937").unwrap();
+        let author = entry.author().unwrap();
+        let title = entry.title().unwrap().format_verbatim();
+        let abstr = entry.abstract_().unwrap().format_verbatim();
+        assert_eq!(author[0].name, "Tolkien");
+        assert_eq!(title, "The Lord of the Rings");
+        assert_eq!(abstr, "An adventure!");
+    }
+}
